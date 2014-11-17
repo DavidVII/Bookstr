0842ea21cc11a725d38a343d91e9c3a8072b96cb
David Becerra <david.becerra@me.com>
2014-11-16 23:32:57 -0800

    Add genre show template

---
+++ app/routes/genre.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  //model: function(params) {
+    //return Ember.RSVP.hash({
+      //genre: this.store.find('genre', params.genre_id),
+      //genres: this.store.findAll('genre')
+    //});
+  //}
+});
